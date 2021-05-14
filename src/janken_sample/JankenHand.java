3686ea0de4512470ee1de7b5e5dee9cd543156ec
hyperkinoko <superkinoko@icloud.com>
2021-05-14 12:25:21 +0900

    3コマ途中

--- src/janken_sample/JankenHand.java
+++ src/janken_sample/JankenHand.java
@@ -4,4 +4,20 @@ public enum JankenHand {
     Rock,
     Scissors,
     Paper;
+
+    public void displayHand() {
+        switch(this) {
+            case Rock:
+                System.out.println("グー");
+                break;
+            case Scissors:
+                System.out.println("チョキ");  
+                break;
+            case Paper:
+                System.out.println("パー");
+                break;
+            default:
+                return;
+        }
+    }
 }
